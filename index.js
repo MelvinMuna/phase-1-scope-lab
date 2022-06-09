5649d2eff11c49b41f37e215168c6d382ead4807
Melvin Muna <melvinmuna@gmail.com>
2022-06-09 23:39:19 +0300

    commit tree 5f29dad99aad5c9b184eba2ece6e616832c459a3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = " customer name"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "customer name";
+  }
